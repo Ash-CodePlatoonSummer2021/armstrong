c1b48a84b7f61ced3d58e8d3c1db00b94c03244a
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-27 12:29:45 -0400

    armstrong

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,13 @@
 # How can you make this more scalable and reusable later?
+import math
+
 def find_armstrong_numbers(numbers):
+    output=[]
+    for x in numbers:
+        digits = str(x)
+        sum=0
+        for i in digits:
+            sum+=int(i)**len(digits)
+        if(x==sum):
+            output.append(x)
+    return output
--- python/armstrong_numbers_spec.py
+++ python/armstrong_numbers_spec.py
@@ -1,7 +1,23 @@
 # The code is written as driver code. Can you convert it to Unit Test?
 from armstrong_numbers import find_armstrong_numbers
+import unittest
 
-print(find_armstrong_numbers([0]) == [0]) 
-print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
-print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+class armstrongTestCase(unittest.TestCase):
+
+    """Tests for `armstrong_numbers.py`"""
+
+    def testZero(self):
+        self.assertEqual(find_armstrong_numbers([0]),[0])
+    def test8(self):
+        self.assertEqual(find_armstrong_numbers(list(range(0, 8))),[0, 1, 2, 3, 4, 5, 6, 7])
+    def test100(self):
+        self.assertEqual(find_armstrong_numbers(list(range(0, 100))),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    def test1000(self):
+        self.assertEqual(find_armstrong_numbers(list(range(0, 999))),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+
+
+t =  armstrongTestCase()
+t.testZero()
+t.test8()
+t.test100()
+t.test1000()
